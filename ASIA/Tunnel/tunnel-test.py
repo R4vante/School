9ef7fd92fdc1abb0ec23967c5c1f9e2b0c83730c
R4vante <l.teegelbeckers@hotmail.com>
2023-06-05 22:04:52 +0200

    changes

--- ASIA/Tunnel/tunnel-test.py
+++ ASIA/Tunnel/tunnel-test.py
@@ -8,7 +8,7 @@ class Wave_packet:
 # NOTE: Code voor het maken van een golfpacket geven in de opgave. Verder uitleg geven 
 # over hoe de class opgeroepen moet worden.
 
-    def __init__(self, N, x0, sigma0, x_begin, x_end, k0 = 0):
+    def __init__(self, N, x0, sigma0, x_begin, x_end, k0 = 0, bw = 1):
        
         self.N = N
         self.x0 = x0
@@ -16,6 +16,7 @@ class Wave_packet:
         self.x_begin = x_begin
         self.x_end = x_end
         self.k0 = k0
+        self.bw = bw
         self.x, self.dx = np.linspace(self.x_begin, self.x_end, self.N, retstep=True)        
 
     def packet(self):
@@ -26,12 +27,12 @@ class Wave_packet:
 #---------------------------
 # NOTE: Alles hieronder niet meeleveren in de opgave
     def potential(self):
-        return 1 * ((self.x >= -0.5) * (self.x <=0.5)).astype(float)
-        # a = -1/(0.5 + 0.5)
-        # k1 = (3+0.5 + a *(self.x + 0.5)) * ((self.x>=-0.5) * (self.x <= 0.5))
-        # k2 = (1+1)* ((self.x>=self.x_begin) * (self.x < -0.5))
-        # return k1+k2
-        # 
+        # return 1 * ((self.x >= -self.bw/2) * (self.x < self.bw/2)).astype(float)    # Potential barrier
+        a = -0.5/(0.5 + 0.5)
+        k1 = (2+0.5 + a *(self.x + 0.5)) * ((self.x>=-self.bw/2) * (self.x <= self.bw/2))
+        k2 = (1+1)* ((self.x>=self.x_begin) * (self.x < -self.bw/2))
+        return k1+k2
+
     def get_e(self):
 
         self.V = self.potential()
@@ -64,11 +65,44 @@ class Wave_packet:
         self.psi_t = self.psi_t / np.sqrt(norm)
         return self.psi_t
 
+    def get_T(self):
+        
+        E, psi = self.get_e()
+        E = E.reshape(-1,1)
+
+        V = self.potential()
+
+        V0 = np.max(V)
+
+        self.T = []
+
+        for i in range(len(E)):
+            
+            c = np.abs(self.cn[i])**2
+            
+            if E[i] < V0:
+                k1 = np.sqrt(2 * (V0 - c*E[i])*self.bw)
+                k2 = 1/4 * V0**2/(c*E[i] * (V0 - c*E[i]))
+                Tt = (1 + k2 * np.sinh(k1)**2)**(-1)
+
+                self.T.append(Tt)
+
+            else:
+                k1 = np.sqrt(2 * (V0 - c*E[i])*self.bw)
+                k2 = 1/4 * V0**2/(c*E[i] * (V0 - c*E[i]))
+                Tt = (1 + k2 * np.sinh(k1)**2)**(-1)
+
+                self.T.append(Tt)
+
+
+
+        return self.T
+
+
 
 def main():
    
-    packet = Wave_packet(1000, -20, 5, -150, 150, k0=-1)
-    t = np.linspace(0, 25, 1000)
+    packet = Wave_packet(1000, -20, 5, -150, 150, k0=-1, bw=2)
 
     psi0 = packet.packet()
 
@@ -88,15 +122,34 @@ def main():
     ani = FuncAnimation(fig, animate, frames = 100, interval=50, blit=False)
     # ani.save('test.mp4', fps=30, dpi=100)
 
-    psi_l = packet.get_t(25)[packet.x <=0]
-    psi_r = packet.get_t(25)[packet.x > 0]
+    psi_l = packet.get_t(60)[packet.x <=0]
+    psi_r = packet.get_t(60)[packet.x > 0]
+    T = np.sum(np.abs(psi_r)**2 * packet.dx)/np.sum(np.abs(packet.get_t(60)**2) * packet.dx)
+    R = np.sum(np.abs(psi_l)**2 * packet.dx)/np.sum(np.abs(packet.get_t(60)**2) * packet.dx)
     E, psi = packet.get_e()
+    
 
-    T = np.sum(np.abs(psi_r)**2 * packet.dx)/np.sum(np.abs(packet.get_t(25)**2) * packet.dx)
-    R = np.sum(np.abs(psi_l)**2 * packet.dx)/np.sum(np.abs(packet.get_t(25)**2) * packet.dx)
-    a = np.sqrt(2*(np.max(packet.potential() - packet.cn**2 * E)))
-    Tt = np.sum((1 + 1/4 * (np.max(packet.potential())**2)/(packet.cn**2*E *(-np.max(packet.potential())+packet.cn**2*E))*np.sinh(a)**2 + 1e-6)**(-1))
-    print(T, Tt)
+    bw = np.linspace(0.01, 3, 10)
+
+    T_i = []
+
+    for i in bw:
+        packet = Wave_packet(1000, -20, 5, -150, 150, k0=-1, bw=i)
+
+        psi_l = packet.get_t(60)[packet.x <=0]
+        psi_r = packet.get_t(60)[packet.x > 0]
+        T = np.sum(np.abs(psi_r)**2 * packet.dx)/np.sum(np.abs(packet.get_t(120)**2) * packet.dx)
+        R = np.sum(np.abs(psi_l)**2 * packet.dx)/np.sum(np.abs(packet.get_t(120)**2) * packet.dx)
+
+        T_i.append(T)
+
+         
+
+    plt.figure()
+
+    plt.scatter(bw, T_i)
+
+
 
     plt.show()
 
--- MSS/Practicum/main.py
+++ MSS/Practicum/main.py
@@ -11,6 +11,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("agg")
 plt.style.use(["seaborn-v0_8-colorblind"])
 import resultaten
 
